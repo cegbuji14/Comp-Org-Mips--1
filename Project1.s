51c67e03e5fd1d79b610e74d12f85627f583dded
cegbuji14 <57194902+cegbuji14@users.noreply.github.com>
2019-10-31 21:37:14 -0400

    Made indention and spacing changes because program ran in mars but not qtspim

--- Project1.s
+++ Project1.s
@@ -1,31 +1,30 @@
 .data
     userInput: .space 11
 .text
-  main:
-  #Lets users input a 10 character string
-    li $v0, 8
-    la $a0, userInput
-    li $a1, 11
-    syscall 
+    main:
+    #Lets users input a 10 character string
+      li $v0, 8
+      la $a0, userInput
+      li $a1, 11
+      syscall 
     
-   start:
-   #Loads the address of inputted string into register $t1, puts first bit (character) of string in $s0
-    la $t1, userInput
-    add $t1, $t1, $t0
-    lb $s0, ($t1)
-    beq $t0, 10, exit #checks if it is 10th character
+    start:
+    #Loads the address of inputted string into register $t1, puts first bit (character) of string in $s0
+      la $t1, userInput
+      add $t1, $t1, $t0
+      lb $s0, ($t1)
+      beq $t0, 10, exit #checks if it is 10th character
 
-    ble $s0, 57, integers #function for the numbers 0-9  integers
-    bge $s0, 97, lowerASCI #function for asci values a-y   lowerASCI
-    bge $s0, 65, UpperASCI #function for asci values A-Y   UpperASCI
-    j exit
-    #beq $t0, 10, exit #function to exit and print the sum
+      ble $s0, 57, integers #function for the numbers 0-9  integers
+      bge $s0, 97, lowerASCI #function for asci values a-y   lowerASCI
+      bge $s0, 65, UpperASCI #function for asci values A-Y   UpperASCI
+      j exit
     
-    addi $t0,$t0, 1
-    j start
+      addi $t0,$t0, 1
+      j start
     
     
-    integers: 
+    integers:
       addi $t0, $t0, 1 #adds one to the incrementer 
       ble $s0, 47, start #if its less than or equal to 47 it is not in the range of what numbers we want, starts loop for next character
       sub $s7, $s0, 48 #subtracts 48 from the asci value to represent the decimal  number and store it in $s7
@@ -38,7 +37,7 @@
       bge $s0, 122, start #if it is greater than or equal to 122 that would represent 'z' and my program is only supposed to go to 'y'
       sub $s4, $s0, 87 #subtracts 87 from asci value to represent the corresponding decimal in the program instructions
       add $s6, $s6, $s4 #adds the decimal number to $s6 where sum is being calculated
-      j start
+       j start
     
     UpperASCI:
       addi $t0, $t0, 1 #incrementer
@@ -57,5 +56,4 @@
 
   
   
-
 
